a49d8882cd4031e1a3b9951768a88b12a433f1b8
vdae2304 <vdae_2304@hotmail.com>
2018-02-05 17:41:27 -0600

    Contest archive

--- OMI/OMI 2017/Dia 2/Esperanza.cpp
+++ OMI/OMI 2017/Dia 2/Esperanza.cpp
@@ -1,40 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef pair<int, int> pii;
-#define MAXN 1000007
+#define MAXN 2000000
 
 int N, dX[] = {-1, 0, 0, 1}, dY[] = {0, -1, 1, 0};
 char c, d[] = {'U', 'L', 'R', 'D'};
-vector<pii> vis[MAXN];
 pii P;
 
-bool Find(pii p) {
-    int Hash = abs(p.first * p.second - p.first ^ p.second) % MAXN;
-    return find(vis[Hash].begin(), vis[Hash].end(), p) != vis[Hash].end();
-}
-
-void Insert(pii &p) {
-    int Hash = abs(p.first * p.second - p.first ^ p.second) % MAXN;
-    vis[Hash].push_back(p);
-}
+struct Hash {
+    size_t operator()(const pii &p) const {
+        return abs(p.first * p.second - ~(p.second ^ p.first)) % MAXN;
+    }
+};
+unordered_set<pii, Hash> vis(MAXN);
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie();
     cin >> N;
+    vis.insert(P);
 
-    Insert(P);
     while (N--) {
         cin >> c;
         int nxt = find(d, d + 4, c) - d;
         P = pii(P.first + dX[nxt], P.second + dY[nxt]);
 
         for (int i = 0; i < 4; i++)
-            if (i != 3 - nxt && Find(pii(P.first + dX[i], P.second + dY[i]))) {
+            if (i != 3 - nxt && vis.count(pii(P.first + dX[i], P.second + dY[i]))) {
                 cout << "N";
                 return 0;
             }
 
-        Insert(P);
+        vis.insert(P);
     }
 
     cout << "S";
